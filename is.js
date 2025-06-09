b45aef774f2b347c09354974903c761a919e225c
ivrontos <johnvrontos96@gmail.com>
2025-06-10 00:13:41 +0300

    NaN diff logic

--- is.js
+++ is.js
@@ -1,13 +1,11 @@
-const is = {}
-
 is.num = (n) => typeof(n)==='number';
-is.nan = (n) => typeof(n)==='NaN';
+is.nan = (n) => Number.isNaN(n);
 is.str = (n) => typeof(n)==='string';
 is.bool = (n) => typeof(n)==='boolean';
 is.undef = (n) => typeof(n)===undefined;
 is.def = (n) => typeof(n)!==undefined;
 is.arr = (n) => typeof(n)==='array';
 is.obj = (n) => typeof(n)==='object';
-is.fun = (n) => (typeof(n)==="function" ? true : false);
+is.fun = (n) => typeof(n)==='function';
 is.truthy = (n) => Boolean(n);
 is.falsy = (n) => !n;
