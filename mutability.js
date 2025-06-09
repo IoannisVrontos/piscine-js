99fff6a669185a2335cbb0b68b68b888af69b128
ivrontos <johnvrontos96@gmail.com>
2025-06-09 14:33:05 +0300

    Mutability on JS

---
+++ mutability.js
@@ -0,0 +1,18 @@
+class Person {
+    constructor(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+}
+
+const person = new Person('Rick',77,'US')
+
+const samePerson = person
+
+const clone1 = new Person('Rick',77,'US')
+
+const clone2 = clone1
+
+person.age++
+person.country = 'FR'
