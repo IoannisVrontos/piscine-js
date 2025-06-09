3e5883abb5f848b2f75a195f624d08d04db2218c
ivrontos <johnvrontos96@gmail.com>
2025-06-09 14:42:04 +0300

    Different approach

--- mutability.js
+++ mutability.js
@@ -1,16 +1,6 @@
-class Person {
-    constructor(name, age, country) {
-        this.name = name;
-        this.age = age;
-        this.country = country;
-    }
-}
+const clone1 = { ...person };
+const clone2 = { ...person };
+const samePerson = person;
 
-const samePerson = person
-
-const clone1 = new Person('Rick',77,'US')
-
-const clone2 = clone1
-
-person.age++
-person.country = 'FR'
+person.age++;
+person.country = 'FR';
