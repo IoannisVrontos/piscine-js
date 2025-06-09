a5fa65653598d084330fad467781369d64a7206c
ivrontos <johnvrontos96@gmail.com>
2025-06-09 16:19:59 +0300

    Init method man quest

---
+++ method-man.js
@@ -0,0 +1,5 @@
+const words = (str) => str.split(' ');
+const sentence = (str) => str.join(' ');
+const yell = (str) => str.toUpperCase();
+const whisper = (str) => "*"+str.toLowerCase()+"*";
+const capitalize = (str) => str[0].toUpperCase()+str.slice(1);
