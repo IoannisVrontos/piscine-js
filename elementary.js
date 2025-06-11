8cee309c7707f3a4dc3cfc6f1364fe61189edca2
ivrontos <johnvrontos96@gmail.com>
2025-06-11 13:42:33 +0300

    Change the logic

--- elementary.js
+++ elementary.js
@@ -1,27 +1,58 @@
-const multiply = (a,b) => {
-    a = Number(a)
-    let res = 0
-    while(b>0){
-        res += a
-        b -= 1
+function sign(n){
+    if (n === 0){
+        return 0
     }
-    return res
+    if (n < 0){
+        return -1
+    }
+    return 1
 }
 
-const divide = (a,b) => {
-    a = Number(a)
-    let count = 0
-    for(let i = a;i>=b;i-=b) {
-        count += 1
+const isPositive = (num) => (num <= 0 ? false : true);
+
+const abs = (num) => (isPositive(num) || num === 0 ? num : num*-1);
+
+const multiply = (a, b) => {
+    const signA = sign(a);
+    const signB = sign(b);
+    const resultSign = signA * signB;
+    a = abs(a);
+    b = abs(b);
+    a = Number(a);
+    b = Number(b);
+    let res = 0;
+    for (let i = 0; i < b; i++) {
+        res += a;
     }
-    return count
+    return resultSign === -1 ? -res : res;
 }
 
-const modulo = (a,b) => {
-    let temp = a 
-    a = Number(a)
-    for(let i = a;i>=b;i-=b) {
-        temp -= b
+const divide = (a, b) => {
+    if (b === 0) return NaN;
+    const signA = sign(a);
+    const signB = sign(b);
+    const resultSign = signA * signB;
+    a = abs(a);
+    b = abs(b);
+    a = Number(a);
+    b = Number(b);
+    let count = 0;
+    while (a >= b) {
+        a -= b;
+        count += 1;
     }
-    return temp
+    return resultSign === -1 ? -count : count;
+}
+
+const modulo = (a, b) => {
+    if (b === 0) return NaN;
+    const signA = sign(a);
+    a = abs(a);
+    b = abs(b);
+    a = Number(a);
+    b = Number(b);
+    while (a >= b) {
+        a -= b;
+    }
+    return signA === -1 ? -a : a;
 }
