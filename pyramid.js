da4788cd38a647b674b863c17ae492d6ecb3edf5
IoannisVrontos <johnvrontos96@gmail.com>
2025-06-14 01:51:32 +0300

    pyr inti

---
+++ pyramid.js
@@ -0,0 +1,20 @@
+function pyramid(str,height) {
+    if (height < 1) {
+    return "";
+  }
+  let result = "";
+  for (let i = 1; i <= height; i++) {
+    for (let j = 1; j <= height - i; j++) {
+      result += " ";
+    }
+    for (let j = 1; j <= 2 * i - 1; j++) {
+      result += str;
+    }
+    if (i < height) {
+      result += "\n";
+    }
+  }
+  return result;
+}
+
+console.log(pyramid('j',5));
