cc9164810b18bf5c78b5dcfdb4de3306a288b34c
ivrontos <johnvrontos96@gmail.com>
2025-06-13 13:28:00 +0300

    correct the calculate the right index

--- slicer.js
+++ slicer.js
@@ -1,9 +1,13 @@
-function slice (elem, startIndx = 0, endIndx = elem.length - 1){
+function slice (elem, startIndx = 0, endIndx = elem.length){
     let newSlice = []
     let count = 0
-    for (let i=startIndx;i<endIndx;i++){
+    for (let i=startIndx;i<=endIndx-1;i++){
         newSlice[count] = elem[i]
         count++
     }
     return newSlice
 }
+
+// const animals = ["ant", "bison", "camel", "duck", "elephant"];
+
+// console.log(slice(animals,1,5))
