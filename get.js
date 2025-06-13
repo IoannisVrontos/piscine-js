741c803c61b0a61db62113237c69003148cec405
IoannisVrontos <johnvrontos96@gmail.com>
2025-06-14 01:25:20 +0300

    Get fixed

--- get.js
+++ get.js
@@ -2,16 +2,18 @@
 // const path = 'nested.key'
 // get(src, path) // -> 'peekaboo'
 
+// get({ a: [{ b: t }] }, 'a.0.b.toString') === t.toString
+
 function get(src,path){
     const parts = path.split('.');
     let current = src;
     
-    for (const part of parts) {
-        if (current && typeof current === 'object' && part in current) {
-        current = current[part];
-        } else {
-        return undefined; 
+    for (let i = 0; i < parts.length; i++) {
+        const part = parts[i];
+        if (current === undefined || current === null) {
+            return undefined;
         }
+        current = current[part];
     }
     
     return current; 
