852b51d3672dfac0ef0157e411c97f0718ab91ea
ivrontos <johnvrontos96@gmail.com>
2025-06-13 19:46:29 +0300

    chunkys init

---
+++ chunky.js
@@ -0,0 +1,12 @@
+function chunky(nums = [], size) {
+    let chunks = [];
+    let arr = [];
+    for (let i = 0; i < nums.length; i += size) {
+        for (let j = i; j < i + size && j < nums.length; j++) {
+            arr.push(nums[j]);
+        }
+        chunks.push(arr);
+        arr = [];
+    }
+    return chunks;
+}
