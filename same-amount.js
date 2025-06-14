2d211948057739f4cf2dc832119e7129a4f8864c
IoannisVrontos <johnvrontos96@gmail.com>
2025-06-14 03:43:27 +0300

    try diff  approach

--- same-amount.js
+++ same-amount.js
@@ -5,9 +5,20 @@
 // The objective is to confirm that the regular expressions 
 // match the string the same number of times.
 
-function sameAmount(str,reg1,reg2){
-    const matches1 = str.match(reg1);
-    const matches2 = str.match(reg2);
+function sameAmount(str, regExp1, regExp2) {
+    const ensureGlobal = (regex) => {
+        let flags = regex.flags;
+        if (!flags.includes('g')) {
+            flags += 'g';
+        }
+        return new RegExp(regex.source, flags);
+    };
+
+    const globalReg1 = ensureGlobal(regExp1);
+    const globalReg2 = ensureGlobal(regExp2);
+
+    const matches1 = str.match(globalReg1);
+    const matches2 = str.match(globalReg2);
 
     const count1 = matches1 ? matches1.length : 0;
     const count2 = matches2 ? matches2.length : 0;
