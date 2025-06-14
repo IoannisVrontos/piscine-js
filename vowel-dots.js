a8f9c5ed573ddcfebca95aa38f084483942c1ea0
IoannisVrontos <johnvrontos96@gmail.com>
2025-06-14 03:25:51 +0300

    voweldots init

---
+++ vowel-dots.js
@@ -0,0 +1,12 @@
+// Create a function named vowelDots that receives a string. 
+// Your function should return a new string with a . after every vowel.
+
+// Your RegEx should be stored in a variable named vowels.
+
+// a, e, i, o and u are considered as vowels here.
+
+function vowelDots(str) {
+    const vowels = /[aeiou]/gi;
+  
+    return str.replace(vowels, '$&.');
+}
