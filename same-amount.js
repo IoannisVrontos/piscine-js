ba04ac980ebd0bada8343f0d350254e671971b5d
IoannisVrontos <johnvrontos96@gmail.com>
2025-06-14 03:36:07 +0300

    Fix logic because match method can return null value

--- same-amount.js
+++ same-amount.js
@@ -8,6 +8,9 @@
 function sameAmount(str,reg1,reg2){
     const matches1 = str.match(reg1);
     const matches2 = str.match(reg2);
-    return matches1.length === matches2.length;
-}
 
+    const count1 = matches1 ? matches1.length : 0;
+    const count2 = matches2 ? matches2.length : 0;
+
+    return count1 === count2;
+}
