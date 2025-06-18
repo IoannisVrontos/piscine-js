66c26af1740e6bb909f380fffc88201aea2d5693
ivrontos <johnvrontos96@gmail.com>
2025-06-18 18:47:10 +0300

    LW #1

---
+++ long-words.js
@@ -0,0 +1,11 @@
+function longWords(array){
+    return array.every((item) => typeof item === 'string' && item.length >= 5);
+}
+
+function oneLongWord(array){
+    return array.some((item) => typeof item === 'string' && item.length >= 10);
+}
+
+function noLongWords(array){
+    return array.every((item) => typeof item === 'string' && !(item.length >= 7));
+}
