0779346fae0a27c596f22077c4b83bf6cb3f4472
ivrontos <johnvrontos96@gmail.com>
2025-06-18 20:26:11 +0300

    partition

--- filter.js
+++ filter.js
@@ -9,11 +9,23 @@ function filter(arr=[],func){
 }
 
 function reject(arr=[],func){
-    const newArr = []; 
+    const newArr = [];
     for(let i=0; i < arr.length; i++){
         if (!func(arr[i],i,arr)){
             newArr.push(arr[i]);
         }
     }
     return newArr; 
+}
+
+function partition(arr=[],func){
+    const resultArr = [[]];
+    for(let i=0; i < arr.length; i++){
+        if (func(arr[i],i,arr)){
+            resultArr[0].push(arr[i]);
+        } else {
+            resultArr[1].push(arr[i]);
+        }
+    }
+    return resultArr; 
 }
