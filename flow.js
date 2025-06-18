bf66a8f35e67cee7fd27bd9c74af64087610866d
ivrontos <johnvrontos96@gmail.com>
2025-06-18 23:24:29 +0300

    Flow #3 Give multiple args

--- flow.js
+++ flow.js
@@ -1,5 +1,9 @@
 function flow(functions) {
-  return function (input) {
-    return functions.reduce((acc, fn) => fn(acc), input);
+  return function (...args) {
+    let result = functions[0](...args);
+    for (let i = 1; i < functions.length; i++) {
+      result = functions[i](result);
+    }
+    return result;
   };
 }
