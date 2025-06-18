cd556e8a3cd2eae6bb0d58c89c29d11fc4a99894
ivrontos <johnvrontos96@gmail.com>
2025-06-18 20:04:44 +0300

    Flat Map #1

--- mapper.js
+++ mapper.js
@@ -5,3 +5,18 @@ function map(arr=[], func){
     }
     return newArr; 
 }
+
+function flatMap(arr = [], func) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        const mappedValue = func(arr[i], i, arr);
+        if (Array.isArray(mappedValue)) {
+            for (let j = 0; j < mappedValue.length; j++) {
+                result.push(mappedValue[j]);
+            }
+        } else {
+            result.push(mappedValue);
+        }
+    }
+    return result;
+}
