bc6f3809a8180588220e5ba38d89cd0b2130f63e
ivrontos <johnvrontos96@gmail.com>
2025-06-18 21:52:01 +0300

    FoldR #1

--- chunky.js
+++ chunky.js
@@ -1,12 +1,12 @@
 function chunk(nums = [], size) {
-    let chunks = [];
-    let arr = [];
-    for (let i = 0; i < nums.length; i += size) {
-        for (let j = i; j < i + size && j < nums.length; j++) {
-            arr.push(nums[j]);
-        }
-        chunks.push(arr);
-        arr = [];
+  let chunks = [];
+  let arr = [];
+  for (let i = 0; i < nums.length; i += size) {
+    for (let j = i; j < i + size && j < nums.length; j++) {
+      arr.push(nums[j]);
     }
-    return chunks;
+    chunks.push(arr);
+    arr = [];
+  }
+  return chunks;
 }
--- reduce.js
+++ reduce.js
@@ -1,19 +1,19 @@
-function fold(array,func,accumulator){
-    let result = accumulator
-    for(let i=0;i<array.length;i++){
-        result = func(result,array[i],i,array);
-    }
-    return result
+function fold(array, func, accumulator) {
+  let result = accumulator;
+  for (let i = 0; i < array.length; i++) {
+    result = func(result, array[i], i, array);
+  }
+  return result;
 }
 
-function foldRight(array,func,accumulator){
-
+function foldRight(array, func, accumulator) {
+  let result = accumulator;
+  for (let i = array.length - 1; i >= 0; i--) {
+    result = func(result, array[i], i, array);
+  }
+  return result;
 }
 
-function reduce(array,func){
+function reduce(array, func) {}
 
-}
-
-function reduceRight(array,func){
-    
-}
+function reduceRight(array, func) {}
