e2c37d7dd36cb7a901cb367a5cb355a748a05c4b
ivrontos <johnvrontos96@gmail.com>
2025-06-19 19:20:43 +0300

    GL

---
+++ harder-bigger-bolder-stronger.js
@@ -0,0 +1,19 @@
+export function generateLetters() {
+  let fontSize = 11;
+  for (let i = 0; i < 120; i++) {
+    const body = document.querySelector("body");
+    const newDiv = document.createElement("div");
+    const letter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    newDiv.style.fontSize = fontSize + "px";
+    if (i <= 40) {
+      newDiv.style.fontWeight = 300;
+    } else if (i > 40 && i <= 80) {
+      newDiv.style.fontWeight = 400;
+    } else {
+      newDiv.style.fontWeight = 600;
+    }
+    fontSize++;
+    newDiv.append(letter);
+    body.append(newDiv);
+  }
+}
